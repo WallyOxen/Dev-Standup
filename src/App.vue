4aa2b59c1170ec5d70a38c1aaf7140c46278c967
WallyOxen <wally@wallys.world>
2022-02-17 20:30:38 -0500

    feat(App): Create localstorage version

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,32 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      class="pt-4"
+      color="grey lighten-3"
+      mini-variant
+    >
+      <v-avatar
+        v-for="n in 2"
+        :key="n"
+        :color="`grey ${n === 1 ? 'darken' : 'lighten'}-1`"
+        :size="n === 1 ? 36 : 20"
+        class="d-block text-center mx-auto mb-9"
+      ></v-avatar>
+    </v-navigation-drawer>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+export default {
+  name: "dev-standup",
+  data: () => ({
+    drawer: true,
+  }),
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,213 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="home">
+    <v-card>
+      <v-card-title class="d-flex justify-center">
+        {{ dayjs(new Date()).format("dddd MM/DD") }}
+      </v-card-title>
+      <v-card-text>
+        <v-list>
+          <template v-for="(dev, index) in devs">
+            <v-list-group :key="index">
+              <template v-slot:activator>
+                <v-list-item-content>
+                  <v-list-item-title class="d-flex justify-center text-h5 pb-2">
+                    {{ dev.name }}
+                    <v-icon
+                      class="ml-4"
+                      @click.stop="out(index)"
+                      color="warning"
+                    >
+                      mdi-account-arrow-right-outline
+                    </v-icon>
+                  </v-list-item-title>
+                </v-list-item-content>
+                <v-list-item-action
+                  v-if="!devs[index].out && done[index]"
+                  style="position: absolute"
+                >
+                  <v-icon color="success" large> mdi-check </v-icon>
+                </v-list-item-action>
+                <v-list-item-action
+                  v-if="devs[index].out"
+                  style="position: absolute"
+                >
+                  <v-icon color="error" large> mdi-close </v-icon>
+                </v-list-item-action>
+              </template>
+              <v-list-item-content
+                class="d-flex flex-column justify-space-around"
+              >
+                <v-text-field
+                  v-model="dev.yesterday"
+                  label="Yesterday"
+                  style="width: 50%"
+                  v-if="dayjs().day() !== 4"
+                />
+                <v-text-field
+                  v-model="dev.today"
+                  label="Today"
+                  style="width: 50%"
+                />
+                <v-text-field
+                  v-model="dev.blockers"
+                  label="Blockers"
+                  style="width: 50%"
+                  append-outer-icon="mdi-cancel"
+                  @click:append-outer="noBlockers(index)"
+                />
+              </v-list-item-content>
+            </v-list-group>
+            <v-divider :key="index + 'd'" />
+          </template>
+        </v-list>
+        <v-card-actions class="d-flex justify-center">
+          <v-btn
+            color="primary"
+            :disabled="!done.every((e) => e)"
+            @click="save"
+          >
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card-text>
+    </v-card>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import dayjs from "dayjs";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  data: () => ({
+    dayjs,
+    previousData: [],
+    devs: [
+      {
+        name: "Tyler",
+        yesterday: "",
+        today: "",
+        blockers: "",
+      },
+      {
+        name: "Gerald",
+        yesterday: "",
+        today: "",
+        blockers: "",
+      },
+      {
+        name: "Matthew",
+        yesterday: "",
+        today: "",
+        blockers: "",
+      },
+      {
+        name: "Michael",
+        yesterday: "",
+        today: "",
+        blockers: "",
+      },
+      {
+        name: "Kody",
+        yesterday: "",
+        today: "",
+        blockers: "",
+      },
+      {
+        name: "Drew",
+        yesterday: "",
+        today: "",
+        blockers: "",
+      },
+      {
+        name: "Andrew",
+        yesterday: "",
+        today: "",
+        blockers: "",
+      },
+      {
+        name: "Ayman",
+        yesterday: "",
+        today: "",
+        blockers: "",
+      },
+      {
+        name: "Christopher",
+        yesterday: "",
+        today: "",
+        blockers: "",
+      },
+      {
+        name: "Jeff",
+        yesterday: "",
+        today: "",
+        blockers: "",
+      },
+    ],
+  }),
+  mounted() {
+    this.previousData = JSON.parse(
+      window.localStorage.getItem("dailyDevNotes")
+    );
+    if (this.previousData === null) {
+      this.previousData = [];
+    }
+    if (this.hasTodaySaved()) {
+      this.$set(
+        this,
+        "devs",
+        this.previousData[this.previousData.length - 1].devs
+      );
+    }
+    if (dayjs().day() === 4) {
+      this.devs.forEach((dev) => (dev.yesterday = "MONDAY"));
+    }
+  },
+  methods: {
+    noBlockers(index) {
+      this.devs[index].blockers = "N/A";
+    },
+    out(index) {
+      const status = this.devs[index].out ? "" : "OUT";
+      this.devs[index].yesterday = status;
+      this.devs[index].today = status;
+      this.devs[index].blockers = status;
+      this.devs[index].out = !this.devs[index].out;
+    },
+    hasTodaySaved() {
+      return (
+        this.previousData.length > 0 &&
+        this.previousData[this.previousData.length - 1].date ===
+          dayjs().format("MM/DD")
+      );
+    },
+    save() {
+      if (this.hasTodaySaved()) {
+        this.previousData[this.previousData.length - 1].devs = this.devs;
+      } else {
+        this.previousData.push({
+          date: dayjs().format("MM/DD"),
+          devs: this.devs,
+        });
+      }
+      window.localStorage.setItem(
+        "dailyDevNotes",
+        JSON.stringify(this.previousData)
+      );
+    },
+  },
+  computed: {
+    done() {
+      return this.devs.map(
+        (dev) => dev.yesterday !== "" && dev.today !== "" && dev.blockers !== ""
+      );
+    },
   },
 };
 </script>
+
+<style scoped>
+#home {
+  height: 100%;
+}
+</style>
