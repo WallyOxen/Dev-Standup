c08883ed1ff6b9cdd09f1ba3d2163d7300005300
WallyOxen <wally@wallys.world>
2022-02-20 20:07:32 -0500

    feat(App): Add icons to nav bar

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,13 @@
       mini-variant
     >
       <router-link v-for="route in routes" :key="route.path" :to="route.path">
-        <v-avatar
+        <v-icon
           :color="`grey ${route.path === $route.path ? 'darken' : 'lighten'}-1`"
           :size="route.path === $route.path ? 36 : 20"
           class="d-block text-center mx-auto mb-9"
         >
-        </v-avatar>
+          {{ route.icon }}
+        </v-icon>
       </router-link>
     </v-navigation-drawer>
 
@@ -32,10 +33,12 @@ export default {
       {
         name: "Notes",
         path: "/",
+        icon: "mdi-text",
       },
       {
         name: "Users",
         path: "/users",
+        icon: "mdi-account-outline",
       },
     ],
   }),
